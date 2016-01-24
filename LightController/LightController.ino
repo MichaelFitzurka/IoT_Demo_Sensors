ea7ff91b02a47b13fd9f5ade01e50c851437e512
Patrick Steiner <patrick@steiner-buchholz.de>
2016-01-24 08:47:47 +0100

    solves and closes #2

--- LightController/LightController.ino
+++ LightController/LightController.ino
@@ -3,7 +3,7 @@
 #include <PubSubClient.h>
 #include <string.h>
 
-/************************* WiFi Access Point *********************************/
+/************************* WiFi Access Point ***************************/
 const char* ssid     = "iotdemo";
 const char* password = "change12_me";
 
@@ -14,11 +14,24 @@ char* mqtt_server           = "192.168.42.1";
 int mqtt_server_port        = 1883;
 const char* mqtt_user       = "admin";
 const char* mqtt_password   = "change12_me";
+String      topicVoltage    = "";
+
+/********************** Global Variables *****************************/
+int           voltage;                    // ESP voltage
+String        message         = "";
+unsigned long count = 0;                  // counter for messagepoints
 
 /************************* Prototypes *********************************/
 void callback(char* topic, byte* payload, unsigned int length);
 
 PubSubClient client(mqtt_server, mqtt_server_port, callback, wifiClient);
+
+/************************* Function to read voltage *******************/
+extern "C" {
+#include "user_interface.h"
+uint16 readvdd33(void);
+}
+
 
 void lightOn() {
   digitalWrite(5, HIGH); 
@@ -43,6 +56,9 @@ void setup() {
   pinMode(5, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   
   lightOff();
+
+  // Create String for MQTT Topics
+  topicVoltage    = "iotdemo/voltage/"+ String( ESP.getChipId() );
   
   // Connect to WiFi network
   WiFi.begin(ssid, password);
@@ -113,4 +129,14 @@ void loop() {
   }
   client.loop();
 
+  voltage = readvdd33();
+  count   = count +1;           // increase counter
+  message = String((int)voltage) + ", " + String(count);
+
+  Serial.print(F("\nSending sensor voltage <"));
+  Serial.print(message);
+  Serial.print(">");
+  client.publish(topicVoltage.c_str(), message.c_str());
+
+  delay(1000);
 }
